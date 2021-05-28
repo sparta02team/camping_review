e3cb7bb406d4280f00548254df2c5eb29080b0c1
JH-debug <oreomasida@gmail.com>
2021-05-29 03:49:14 +0900

    config 파일 사용 설정

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from pymongo import MongoClient
+import os
 
 client = MongoClient('localhost', 27017)
 db = None
@@ -9,13 +10,14 @@ def create_app(database_name='camping_review'):
     # 플라스크 웹 서버 생성하기
     app = Flask(__name__)
     app.debug = True
-    # app.config.from_pyfile('config.py')
+    app.config.from_pyfile('config.py')
 
     global db
     db = client.get_database(database_name)
 
-    from .views import main, api
+    from app.views import main, api, camping_data
     app.register_blueprint(main.bp)
     app.register_blueprint(api.bp)
+    app.register_blueprint(camping_data.bp)
 
     return app
