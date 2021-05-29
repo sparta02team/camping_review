cdc753c6add0411c2a1cfb880ce6f63d7a159070
nbgh46 <nbgh46@naver.com>
2021-05-29 14:30:46 +0900

    회원가입 html main api 작성

--- app/views/main.py
+++ app/views/main.py
@@ -6,3 +6,8 @@ bp = Blueprint('main', __name__, url_prefix='/')
 @bp.route('', methods=['GET'])  # 데코레이터 문법
 def index():  # 함수 이름은 고유해야 한다
     return render_template('index.html')
+
+
+@bp.route('/register', methods=['GET'])
+def register():
+    return render_template('register.html')
