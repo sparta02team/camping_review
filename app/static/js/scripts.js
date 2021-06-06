d0eefe7743126de76e0ef744ffabc8b92a8cbd8f
JH-debug <oreomasida@gmail.com>
2021-06-06 21:30:18 +0900

    camping_data 받아오도록 index.html 연결

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -6,6 +6,7 @@
 
 
 $(document).ready(function () {
+    // user 정보
     setUserInfo()
 });
 
@@ -48,6 +49,18 @@ function logOut() {
 }
 
 
+function to_review() {
+    $.ajax({
+        type: 'POST',
+        url: '/review',
+        data: {'mapx': '300492', 'mapy': '552541', 'title': '난지캠핑장', 'address': '서울특별시 마포구 한강난지로 28'},
+        success: function (response) {
+            window.location.href = '/review'
+        }
+    })
+}
+
+
 function showArticles() {
     let region = $('.form-select').val()
     console.log(region)
@@ -63,26 +76,48 @@ function showArticles() {
                     let article = articles[i]
                     let campsite_name = article['campsite_name']
                     let category = article['category']
+                    let description = article['description']
+                    let image = article['image']
                     let link = article['link']
+                    let phone = article['phone']
                     let address = article['address']
                     let road_address = article['road_address']
-                    makeCard(campsite_name, category, link, address, road_address)
+                    let tag = article['tag']
+                    makeCard(campsite_name, category, description, image, link, phone, address, road_address, tag)
                 }
-            } else {
-                alert('검색에 실패하였습니다.');
             }
+        },
+            error: function (response) {
+                alert('검색에 실패하였습니다.');
         }
     })
 }
 
-function makeCard(campsite_name, category, link, address, road_address) {
+
+function makeCard(campsite_name, category, description, image, link, phone, address, road_address, tag) {
     let tempHtml = `<div class="card">
-            <div class="card-body">
-            <a href="${link}" target="_blank" class="card-title">${campsite_name}</a>
-            <p class="card-text">${category}</p>
-            <p class="card-text comment">${address}</p>
-            <p class="card-text comment">${road_address}</p>
-            </div>
-        </div>`;
+                        <div class="card-header" style="background-image: url('${image}')">
+                            <div class = "card-header-is_closed" ></div>
+                        </div>
+                        <div class="card-body">
+                            <div class="card-body-header">
+                                <span class="card-header">${category}</span>
+                                <h1 class="card-title">${campsite_name}</h1>
+                                <a href="${link}" class="card-title">${link}</a>
+                                <p class="card-text-hashtag">${tag}</p>
+                                <p class = "card-body-phone">${phone}</p>
+                                <p class = "card-body-address">${address}</p>
+                                <p class = "card-body-address">${road_address}</p>
+                            </div>
+                                <p class="card-body-description">
+                                    ${description}
+                                </p>
+                            <div class="card-body-footer">
+                                <hr style="margin-bottom: 8px; opacity: 0.5; border-color: #FF5675">
+                                <i class="icon icon-comment"></i>리뷰 작성
+                                <i class="icon icon-comments_count"></i>리뷰 개수
+                            </div>
+                        </div>
+                    </div>`;
     $("#cards-box").append(tempHtml);
 }
