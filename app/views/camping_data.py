cac09f1fa046ff0b681291c0de8d2e6715beb3cf
JH-debug <oreomasida@gmail.com>
2021-06-06 18:58:06 +0900

    daum 검색 api 이용해 지역별 캠핑장 정보 가져오기

--- app/views/camping_data.py
+++ app/views/camping_data.py
@@ -1,54 +1,61 @@
 import requests
+from selenium import webdriver
+from selenium.webdriver import ActionChains
 from flask import Blueprint, current_app, request, jsonify
 import time
 import html.parser
 import re
-from app import db
+# from app import db
 
 
 bp = Blueprint('camping_data', __name__, url_prefix='/camping_data')
 
 #lists = ["서울시", "부산시", "대구시", "인천시", "광주시", "대전시", "울산시", "세종시", "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도", "경상북도", "경상남도", "제주도"]
 
-@bp.route('/get_result', methods=['GET'])
-def get_naver_result():
-    form = request.form
-    region = form['region_give']
+
+@bp.route('', methods=['GET'])
+def get_result():
+    # form = request.form
+    # region = form['region_give']
+    region = request.args.get('region_give')
 
     time.sleep(0.1)
-    url = f"https://openapi.naver.com/v1/search/local.json?query={region} 캠핑장&display=5&start=1&sort=random"
-    headers = {'X-Naver-Client-Id': current_app.config['CLIENT_ID'],
-               'X-Naver-Client-Secret': current_app.config['CLIENT_SECRET']}
+
+    url = "https://dapi.kakao.com/v2/local/search/keyword.json?query={} 캠핑장&size=10".format(region)
+    headers = {"Authorization": "KakaoAK " + current_app.config['REST_API']}
 
     data = requests.get(url, headers=headers)
-    data = data.json()['items']
-    # print(data)
+    data = data.json()['documents']
 
     documents = []
     if data:
         for d in data:
 
             document = {
-
-                'campsite_name': re.sub('<[^>]*>', ' ', html.unescape(d['title'])),
-                'category' : d['category'],
-                'address' : d['address'],
-                'road_address': d['roadAddress'],
-                'link' : d['link']
+                'campsite_name': re.sub('<[^>]*>', ' ', html.unescape(d['place_name'])),
+                'category' : d['category_name'],
+                'address' : d['address_name'],
+                'road_address' : d['road_address_name'],
+                'phone': d['phone'],
+                'link' : d['place_url'],
+                'x' : d['x'],
+                'y' : d['y']
             }
 
-            db.campsite.insert_one(document)
-
-    return jsonify({'result': 'succcess'})
+            documents.append(document)
 
 
-@bp.route('', methods=['GET'])
-def list_result():
-    data = list(db.campsite.find({}, {'_id': False}))
-    result = {
-        'result': 'success',
-        'articles': data,
-    }
 
-    return jsonify(result)
+    return jsonify({'result': 'success',
+                    'articles': documents})
 
+
+# @bp.route('', methods=['GET'])
+# def list_result():
+#     data = list(db.campsite.find({}, {'_id': False}))
+#     result = {
+#         'result': 'success',
+#         'articles': data,
+#     }
+#
+#     return jsonify(result)
