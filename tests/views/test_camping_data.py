8401e555e11383b95c6e2706484dc49eaaedd7ed
JH-debug <oreomasida@gmail.com>
2021-06-15 01:51:45 +0900

    test_camping_data 코드

--- tests/views/test_camping_data.py
+++ tests/views/test_camping_data.py
@@ -1,3 +1,11 @@
-def test_get_result(client):
-    response = client.get('/camping_data')
+def test_list_result(client):
+    response = client.get('/camping_data/crawling')
+    assert response.status_code == 200
+
+
+def test_list_result(client):
+    data = {
+        'region_give': '서울시'
+    }
+    response = client.get('/camping_data', data=data)
     assert response.status_code == 200
