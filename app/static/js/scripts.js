086e4175816e2b70419362439ae6307be3b08282
JH-debug <oreomasida@gmail.com>
2021-06-16 16:23:49 +0900

    count_review API, 리뷰 개수 화면에 띄우기 원래대로

--- app/static/js/scripts.js
+++ app/static/js/scripts.js
@@ -119,44 +119,53 @@ function showArticles()
 
 function makeCard(campsite_name, category, description, image, link, phone, address, road_address, tag)
 {
-    let tempHtml = `<div class="card">
-                        <div class="card-header" style="background-image: url('${image}')">
-                            <div class = "card-header-is_closed" ></div>
-                        </div>
-                        <div class="card-body">
-                            <div class= "card-body-header">
-                                <span class= "card-category">${category}</span>
-                                <h1 class= "card-title">${campsite_name}</h1>
-                                <a href="${link}"><img class= "card-link" style= "border-style: none;"></a>
-                                <hr style="opacity: 0.4; border-color: #FF5675">
-                                <p class="card-text-hashtag">${tag}</p>
-                                <p class= "card-body-phone">${phone}</p>
-                                <p class= "card-body-address">${address}</p>
-                                <p class= "card-body-road-address">${road_address}</p>
-                                
-                            </div>
-                                <p class="card-body-description">
-                                    ${description}
-                                </p>
-                            <div class="card-body-footer">
-                                <hr style="margin-bottom: 8px; opacity: 0.5; border-color: #FF5675">
-                                <i class="icon icon-comment" onclick="to_review('${campsite_name}')" style='cursor:pointer;' ></i>리뷰 작성
-                                <i class="icon icon-comments_count"></i>리뷰 개수
-                            </div>
-                        </div>
-                    </div>`;
-    $("#cards-box").append(tempHtml);
+    $.ajax({
+    type: "GET",
+    url: "/review/count_review",
+    data: {'camping_site': campsite_name},
+    success: function (response) {
+        if (response["result"] == "success") {
+            let count = response['articles']
+            let tempHtml = `<div class="card">
+                                <div class="card-header" style="background-image: url('${image}')">
+                                    <div class = "card-header-is_closed" ></div>
+                                </div>
+                                <div class="card-body">
+                                    <div class= "card-body-header">
+                                        <span class= "card-category">${category}</span>
+                                        <h1 class= "card-title">${campsite_name}</h1>
+                                        <a href="${link}"><img class= "card-link" style= "border-style: none;"></a>
+                                        <hr style="opacity: 0.4; border-color: #FF5675">
+                                        <p class="card-text-hashtag">${tag}</p>
+                                        <p class= "card-body-phone">${phone}</p>
+                                        <p class= "card-body-address">${address}</p>
+                                        <p class= "card-body-road-address">${road_address}</p>
+                                        
+                                    </div>
+                                        <p class="card-body-description">
+                                            ${description}
+                                        </p>
+                                    <div class="card-body-footer">
+                                        <hr style="margin-bottom: 8px; opacity: 0.5; border-color: #FF5675">
+                                        <i class="icon icon-comment" onclick="to_review('${campsite_name}')"></i>리뷰 작성
+                                        <i class="icon icon-comments_count" title=></i>리뷰 ${count}개
+                                    </div>
+                                </div>
+                            </div>`;
+            $("#cards-box").append(tempHtml);
+            }
+        }
+    })
 }
+
 
 function to_review(campsite_name)
 {
-    let index = $('i.icon icon-comment').index(this)
-    // alert(index)
+    // let index = $(e).parent().parent().parent().index()
+    // let camping_site =  $('h1.card-title:eq('+ index +')').text()
     let camping_site = campsite_name
     console.log(camping_site)
     console.log(id)
-    // let camping_site = $('h1.card-title:eq(0)').text()
-    // alert(camping_site)
 
     $.ajax({
         type: 'POST',
