7b9d91d2ff6312ecaf180b774ee656526e0856e5
JH-debug <oreomasida@gmail.com>
2021-06-16 19:03:04 +0900

    test_camping_data 원래대로

--- tests/views/test_camping_data.py
+++ tests/views/test_camping_data.py
@@ -1,3 +1,23 @@
-def test_get_result(client):
-    response = client.get('/camping_data')
+from pymongo import MongoClient
+
+test_database_name = 'camping_review_test'
+client = MongoClient('localhost', 27017)
+db = client.get_database(test_database_name)
+
+def test_list_result(client):
+    data = {
+        'region_give': '부산시'
+    }
+    response = client.get(
+        '/camping_data',
+        data=data
+    )
+
+    campsite = db.campsite.find_one(
+        {'region': 'region_give'},
+        {'_id': False}
+    )
+
     assert response.status_code == 200
+    assert response.json['result'] == 'success'
+
