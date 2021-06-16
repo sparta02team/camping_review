5ac7eee30afd4f9357828feaf0b0e906658a8395
JH-debug <oreomasida@gmail.com>
2021-06-16 17:21:50 +0900

    카카오 api만 이용해서 캠핑장 정보 바로 추출하도록 수정

--- app/views/camping_data.py
+++ app/views/camping_data.py
@@ -12,91 +12,85 @@ from app import db
 bp = Blueprint('camping_data', __name__, url_prefix='/camping_data')
 
 
-@bp.route('/crawling', methods=['GET'])
+@bp.route('/', methods=['GET'])
 def get_result():
     # form = request.form
     # region = form['region_give']
-    # region = request.args.get('region_give')
+    region = request.args.get('region_give')
 
-    lists = ["서울시", "부산시", "대구시", "인천시", "광주시", "대전시", "울산시", "세종시", "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도",
-             "경상북도", "경상남도", "제주도"]
+    # lists = ["서울시", "부산시", "대구시", "인천시", "광주시", "대전시", "울산시", "세종시", "경기도", "강원도", "충청북도", "충청남도", "전라북도", "전라남도",
+    #          "경상북도", "경상남도", "제주도"]
 
-    db.campsite.remove({})
-
-    for region in lists:
-        url = "https://dapi.kakao.com/v2/local/search/keyword.json?query={} 캠핑장&size=9".format(region)
-        headers = {"Authorization": "KakaoAK " + current_app.config['REST_API']}
-
-        time.sleep(0.1)
-
-        data = requests.get(url, headers=headers)
-        data = data.json()['documents']
-
-        documents = []
-        if data:
-            for d in data:
-
-                document = {
-                    'region': region,
-                    'campsite_name': re.sub('<[^>]*>', ' ', html.unescape(d['place_name'])),
-                    'category' : d['category_name'],
-                    'address' : d['address_name'],
-                    'road_address' : d['road_address_name'],
-                    'phone': d['phone'],
-                    'link' : d['place_url'],
-                    'x' : d['x'],
-                    'y' : d['y']
-                }
-
-                documents.append(document)
-
-        for document in documents:
-            url = document['link']
-            document['tag'] = []
-            document['image'] = ''
-            document['description'] = ''
-
-            chromedriver = os.path.join('chromedriver')
-            # chromedriver = current_app.config['CHROME_DRIVER']
-            options = webdriver.ChromeOptions()
-            options.add_argument("headless")
-            driver = webdriver.Chrome(chromedriver, options=options)
-            driver.get(url)
-            driver.implicitly_wait(0.3)
-
-            try:
-                tags = driver.find_elements_by_xpath("//a[contains(text(),'#') and @class='link_tag']")
-                for tag in tags:
-                    document['tag'].append(tag.text)
-
-                image = driver.find_element_by_css_selector('a.link_photo').get_attribute('style')
-                # action = ActionChains(driver)
-                # action.move_to_element(image).perform()
-                document['image'] = image.split('background-image: url("')[1][:-3]
-
-                description = driver.find_element_by_css_selector('p.txt_introduce')
-                document['description'] = description.text
-
-            except:
-                pass
-
-            driver.close()
-
-        db.campsite.insert_many(documents)
-
-    return jsonify({'result': 'success'})
+    # db.campsite.remove({})
 
 
-@bp.route('', methods=['GET'])
-def list_result():
-    # form = request.form
-    # region = form['region_give']
-    region_give = request.args.get('region_give')
+    url = "https://dapi.kakao.com/v2/local/search/keyword.json?query={} 캠핑장&size=9".format(region)
+    headers = {"Authorization": "KakaoAK " + current_app.config['REST_API']}
 
-    data = list(db.campsite.find({'region': region_give}, {'_id': False}))
+    time.sleep(0.1)
+
+    data = requests.get(url, headers=headers)
+    data = data.json()['documents']
+
+    documents = []
+    if data:
+        for d in data:
+
+            document = {
+                'region': region,
+                'campsite_name': re.sub('<[^>]*>', ' ', html.unescape(d['place_name'])),
+                'category' : d['category_name'],
+                'address' : d['address_name'],
+                'road_address' : d['road_address_name'],
+                'phone': d['phone'],
+                'link' : d['place_url'],
+                'x' : d['x'],
+                'y' : d['y']
+            }
+
+            documents.append(document)
+
+    for document in documents:
+        place_id = document['link'].split('/')[-1]
+        document['tag'] = []
+        document['image'] = ''
+        document['description'] = ''
+
+        URL_DETAILS_PAGE = "https://place.map.kakao.com/main/v/"
+        place_details = requests.get(URL_DETAILS_PAGE + place_id).json()
+
+        try:
+            tags = place_details['basicInfo']['metaKeywordList']
+            for tag in tags:
+                document['tag'].append(tag)
+
+            photo_list = place_details['photo']
+            document['image'] = photo_list['photoList'][0]['list'][0]['orgurl']
+            document['description'] = place_details['basicInfo']['introduction']
+
+        except:
+            pass
+
+            # db.campsite.insert_many(documents)
+
     result = {
-        'result': 'success',
-        'articles': data,
-    }
+            'result': 'success',
+            'articles': documents,
+        }
 
     return jsonify(result)
+
+
+# @bp.route('', methods=['GET'])
+# def list_result():
+#     # form = request.form
+#     # region = form['region_give']
+#     region_give = request.args.get('region_give')
+#
+#     data = list(db.campsite.find({'region': region_give}, {'_id': False}))
+#     result = {
+#         'result': 'success',
+#         'articles': data,
+#     }
+#
+#     return jsonify(result)
